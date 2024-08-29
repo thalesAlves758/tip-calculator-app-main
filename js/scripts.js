b0f66199cd8248575b7652c93d29300f5d15c1b9
Thales Alves <thalesalves758@gmail.com>
2024-08-29 00:34:11 -0300

    add fields functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,81 @@
 
 const tipsButtons = document.querySelectorAll('.tip');
+const billField = document.querySelector('#bill-field');
+const customTipField = document.querySelector('.tip-field');
 const peopleNumberField = document.querySelector('#number-of-people-field');
 
-initTipsButtonsClickFunctions(tipsButtons);
-initPeopleNumberChangeFuntion(peopleNumberField);
+initDecimalFieldsStandardChangeFunctions(billField);
 
-function initTipsButtonsClickFunctions(buttons) {
-    buttons.forEach(button => {
-        button.addEventListener('click', event => {
-            event.target.classList.toggle('selected');
+initIntegerNumberChangeFuntions(customTipField);
 
-            buttons.forEach(unselectedButton => {
-                if(unselectedButton.value !== event.target.value) {
-                    unselectedButton.classList.remove('selected');
-                }
-            })
-        });
+initIntegerNumberChangeFuntions(peopleNumberField);
+initPeopleNumberFieldChangeFunctions(peopleNumberField);
+
+tipsButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        event.target.classList.toggle('selected');
+
+        unselectButtons(tipsButtons, event.target);
+
+        customTipField.value = null;
+    });
+});
+
+customTipField.addEventListener('input', () => {
+    unselectButtons(tipsButtons);
+});
+
+function initDecimalFieldsStandardChangeFunctions(field) {
+    initStandardNumberFieldChangeFunction(field);
+
+    field.addEventListener('keydown', event => {
+        if(
+            (event.key === '.' && !hasValue(event.target.value)) ||
+            (isDecimalFormatAlreadyFilledIn(event.target.value) && event.key !== 'Backspace')
+        ) {
+            event.preventDefault();
+        }
     });
 }
 
-function initPeopleNumberChangeFuntion(field) {
+function hasValue(fieldValue) {
+    return fieldValue !== 0 && !!fieldValue;
+}
+
+function isDecimalFormatAlreadyFilledIn(value) {
+    return /^[0-9]+\.[0-9]{2}$/.test(value);
+}
+
+function initIntegerNumberChangeFuntions(field) {
+    initStandardNumberFieldChangeFunction(field);
+
+    field.addEventListener('keydown', event => {
+        let fullValue = `${event.target.value}${event.key}`;
+        if(event.key === '.' || (event.key !== 'Backspace' && Number(fullValue) > Number(event.target.max))) {
+            event.preventDefault();
+        }
+    });
+}
+
+function initStandardNumberFieldChangeFunction(field) {
+    field.addEventListener('keydown', event => {
+        if(isForbiddenKey(event.key)) {
+            event.preventDefault();
+        }
+    });
+}
+
+function isForbiddenKey(key) {
+    const forbiddenKeys = [
+        '-',
+        '+',
+        ','
+    ];
+
+    return forbiddenKeys.includes(key);
+}
+
+function initPeopleNumberFieldChangeFunctions(field) {
     field.addEventListener('input', event => {
         let fieldGroupParent = event.target.closest('.field-group');
 
@@ -29,5 +85,13 @@ function initPeopleNumberChangeFuntion(field) {
         }
 
         fieldGroupParent.classList.remove('invalid-field');
+    });
+}
+
+function unselectButtons(buttons, selectedButton) {
+    buttons.forEach(button => {
+        if(button.value !== selectedButton?.value) {
+            button.classList.remove('selected');
+        }
     });
 }
